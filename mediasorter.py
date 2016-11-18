986ae1365d7be9abfb5f7fcdac44581ec7610e70
Gonzalo Alvarez <gonzaloab@gmail.com>
2016-11-18 13:38:31 -0800

    Fixed some issues and improved performance

--- mediasorter.py
+++ mediasorter.py
@@ -8,9 +8,15 @@ import sys as Sys
 import os
 import time
 import shutil
+import cProfile
+from datetime import timedelta
+import time
+from pstats import Stats
 from datetime import datetime
 from lib.media import ExifTool
 import magic
+from PIL import Image
+from PIL.ExifTags import TAGS
 
 # https://github.com/andrewning/sortphotos/blob/master/src/sortphotos.py
 # https://github.com/jmathai/elodie/blob/master/elodie/filesystem.py
@@ -75,6 +81,7 @@ class FilesystemHelper(object):
                     imageDateObject = None
         if imageDateObject is None:
             imageDateObject = datetime.strptime('1970-1-1', '%Y-%m-%d')
+            EventHandler.instance().count('unknown_date')
             logging.info('Unable to identify date for [{}]'.format(fileName))
         return imageDateObject
 
@@ -101,25 +108,53 @@ class FilesystemHelper(object):
 
 class MediaHelper(object):
     @staticmethod
-    def getDateFromMediaFile(fileName):
-        if fileName is None:
+    def getDateFromMediaFile(mediaFile):
+        if mediaFile is None or mediaFile.fileName is None:
             return None
         mediaFileDate = None
-        exifMediaFileDate = MediaHelper.getExifByTags(fileName, ('DateTimeOriginal', 'DateTime'))
+        exifMediaFileDate = MediaHelper.getExifByTags(mediaFile, ('DateTimeOriginal', 'DateTime'))
         if exifMediaFileDate is not None:
             mediaFileDate = datetime.strptime(exifMediaFileDate, '%Y:%m:%d %H:%M:%S')
         if mediaFileDate is None:
-            mediaFileDate = FilesystemHelper.getDateFromFileName(fileName)
+            mediaFileDate = FilesystemHelper.getDateFromFileName(mediaFile.fileName)
         return mediaFileDate
 
     @staticmethod
-    def getExifByTags(fileName, tags):
+    def getExifByTags(mediaFile, lookupTags):
+        if mediaFile.isImage:
+            exifValue = MediaHelper.getExifByTagsUsingPIL(mediaFile, lookupTags)
+            if exifValue is not None:
+                return exifValue
+        else:
+            EventHandler.instance().count('Video')
+        return MediaHelper.getExifByTagsUsingExifTool(mediaFile, lookupTags)
+
+    @staticmethod
+    def getExifByTagsUsingExifTool(mediaFile, lookupTags):
         with ExifTool() as et:
-            exifInfo = et.get_metadata(fileName)
-            for key in tags:
+            exifInfo = et.get_metadata(mediaFile.fileName)
+            for key in lookupTags:
                 lookupKey = 'EXIF:' + key
                 if lookupKey in exifInfo:
                     return exifInfo[lookupKey]
+        return None
+
+
+    @staticmethod
+    def getExifByTagsUsingPIL(mediaFile, lookupTags):
+        pilImage = Image.open(mediaFile.fileName)
+        if '_getexif' in dir(pilImage):
+            pilExif = pilImage._getexif()
+            if pilExif is not None:
+                for tag, value in pilExif.items():
+                    for lookupTag in lookupTags:
+                        if TAGS.get(tag) == lookupTag:
+                            return value
+                EventHandler.instance().count('PIL_nodate')
+            else:
+                EventHandler.instance().count('PIL_noexif')
+        else:
+            EventHandler.instance().count('PIL_notsupported')
         return None
 
 class MediaFile(object):
@@ -132,8 +167,9 @@ class MediaFile(object):
         if self.mimeExtension in ('.jpe', '.jpeg'):
             self.mimeExtension = '.jpg'
         self.isMedia = self.mimeType.split('/')[0] in ('video', 'image')
+        self.isImage = self.mimeType.split('/')[0] == 'image'
         if self.isMedia:
-            self.date = MediaHelper.getDateFromMediaFile(fileName)
+            self.date = MediaHelper.getDateFromMediaFile(self)
 
     def __str__(self):
         return 'Name: [{}] Mime: {} Extension: {} Date: {}'.format(self.fileName, self.mimeType, self.mimeExtension, self.date.strftime("%Y-%m-%d %H:%M"))
@@ -147,7 +183,8 @@ class DuplicateMediaFileException(Exception):
 class MediaSorter(object):
     @staticmethod
     def sortFiles(sourceFolder, targetFolder, limitCount = None):
-        uiUtils = UIUtils()
+        uiUtils = UIUtils.instance()
+        uiUtils.outputNoNewLine('Looking up files...')
         mimeMagic = magic.Magic(mime=True, uncompress=True)
         mimetypes.init()
         inputFileList = FilesystemHelper.listFiles(sourceFolder)
@@ -156,17 +193,75 @@ class MediaSorter(object):
             inputFileList = inputFileList[:limitCount]
 
         uiUtils.setProgressTotal(len(inputFileList))
-        uiUtils.outputToUser('Identified {} files'.format(len(inputFileList)))
+        uiUtils.output('Identified {} files'.format(len(inputFileList)))
         for inputFile in inputFileList:
             sourceMediaFile = MediaFile(inputFile)
             if sourceMediaFile.isMedia:
                 try:
                     MediaFileManager.copyMediaFile(sourceMediaFile, targetFolder)
                 except DuplicateMediaFileException as duplicateException:
+                    EventHandler.instance().count('duplicated')
                     logging.warn('File [{}] already exists in destination as [{}]. Skipping.'.format(inputFile, duplicateException.existingFile))
             else:
+                EventHandler.instance().count('unknown_type')
                 logging.warn('File [{}] does not have a recognized media type [{}]. Skipping.'.format(inputFile, sourceMediaFile.mimeExtension))
             uiUtils.incrementProgress()
+
+
+class Singleton:
+    """
+    A non-thread-safe helper class to ease implementing singletons.
+    This should be used as a decorator -- not a metaclass -- to the
+    class that should be a singleton.
+
+    The decorated class can define one `__init__` function that
+    takes only the `self` argument. Also, the decorated class cannot be
+    inherited from. Other than that, there are no restrictions that apply
+    to the decorated class.
+
+    To get the singleton instance, use the `Instance` method. Trying
+    to use `__call__` will result in a `TypeError` being raised.
+
+    """
+
+    def __init__(self, decorated):
+        self._decorated = decorated
+
+    def instance(self):
+        """
+        Returns the singleton instance. Upon its first call, it creates a
+        new instance of the decorated class and calls its `__init__` method.
+        On all subsequent calls, the already created instance is returned.
+
+        """
+        try:
+            return self._instance
+        except AttributeError:
+            self._instance = self._decorated()
+            return self._instance
+
+    def __call__(self):
+        raise TypeError('Singletons must be accessed through `Instance()`.')
+
+    def __instancecheck__(self, inst):
+        return isinstance(inst, self._decorated)
+
+@Singleton
+class EventHandler(object):
+    def __init__(self):
+        self.metrics = {}
+
+    def count(self, event):
+        if event not in self.metrics:
+            self.metrics[event] = 0
+        self.metrics[event] += 1;
+
+    def __str__(self):
+        strings = []
+        for key, value in self.metrics.iteritems():
+            strings.append('Event [{}] count [{}]'.format(key, value))
+        return '\n'.join(strings)
+
 
 class MediaFileManager(object):
     @staticmethod
@@ -202,6 +297,7 @@ class MediaFileManager(object):
         FilesystemHelper.copyFileWithProperties(mediaFile.fileName, targetUniquePath)
 
 
+@Singleton
 class UIUtils(object):
     def __init__(self):
         logging.basicConfig(filename='/tmp/mediasorter.log', filemode='w', format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
@@ -239,8 +335,12 @@ class UIUtils(object):
     def resetProgress(self):
         self.progressBarCurrent = 0
 
-    def outputToUser(self, string):
+    def output(self, string):
         print string
+
+    def outputNoNewLine(self, string):
+        sys.stdout.write('{}\r'.format(str(string)))
+        sys.stdout.flush()
 
 @click.command('import')
 @click.argument('source', type=click.Path(file_okay=True),
@@ -250,8 +350,19 @@ class UIUtils(object):
 @click.option('--limit', type=int, default=None)
 @click.pass_context
 def _import(ctx, source, target, limit):
+    if ctx.obj['DEBUG']:
+        profile = cProfile.Profile()
+        profile.enable()
+        startTime = time.time()
     mediasorter = MediaSorter()
     mediasorter.sortFiles(source, target, limit)
+    if ctx.obj['DEBUG']:
+        profile.disable()
+        UIUtils.instance().output('Time elapsed: {}'.format(str(timedelta(seconds=(time.time() - startTime)))))
+        UIUtils.instance().output('Profiler data:')
+        stats = Stats(profile).sort_stats('time')
+        stats.print_stats(10)
+        UIUtils.instance().output(EventHandler.instance())
 
 @click.group()
 @click.option('--debug/--no-debug', default=False)
