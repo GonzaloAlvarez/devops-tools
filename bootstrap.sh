75338e350185b8ab07c8172bc87810dc2badf751
Gonzalo Alvarez <gonzaloab@gmail.com>
2017-01-20 17:00:44 -0800

    Fixes dependencies model and consolidate bootstrap. Fixes #9 and fixes #8

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,54 +1,76 @@
 #!/bin/bash
 BASE_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ENV_PATH="$BASE_PATH/.env"
-PYTHON_PACKAGES="$BASE_PATH/packages.pip"
+DEPENDENCIES_FILE="$BASE_PATH/dependencies"
 VIRTUALENV_GIT="https://github.com/pypa/virtualenv.git"
 VIRTUALENV_PATH="$ENV_PATH/virtualenv"
 VIRTUALENV="$VIRTUALENV_PATH/virtualenv.py"
+NODE_BASE="$ENV_PATH/lib"
 export GEM_HOME="$ENV_PATH/gems"
 export GEM_PATH=""
-RUBY_PACKAGES="$BASE_PATH/ruby.packages"
-NODE_BASE="$ENV_PATH/lib"
-NODE_PACKAGES="$BASE_PATH/node.packages"
 
 function log() {
     echo $@
 }
 
-if [ ! -d $ENV_PATH ]; then
-    log "Creatin the virtual environment..."
-    mkdir -p "$ENV_PATH"
-    PYTHON=$(which python)
-    GIT=$(which git)
-    log "Retrieving virtualenv"
-    $GIT clone --depth 1 -q "$VIRTUALENV_GIT" "$VIRTUALENV_PATH"
-    log "Executing virtualenv"
-    $PYTHON "$VIRTUALENV" "$ENV_PATH"
-    if [ -r "$PYTHON_PACKAGES" ]; then
-        log "Installing python dependencies"
-        $ENV_PATH/bin/pip install -q -r "$PYTHON_PACKAGES"
+function env_install() {
+    if [ ! -d $ENV_PATH ]; then
+        log "Creatin the virtual environment..."
+        mkdir -p "$ENV_PATH"
     fi
-fi
-if [ ! -d $GEM_HOME ]; then
+}
+
+function python_base() {
+    env_install
+    if [ ! -d "$VIRTUALENV_PATH" ]; then
+        log "Installing virtualenv"
+        PYTHON=$(which python)
+        GIT=$(which git)
+        $GIT clone --depth 1 -q "$VIRTUALENV_GIT" "$VIRTUALENV_PATH"
+        log "Executing virtualenv"
+        $PYTHON "$VIRTUALENV" "$ENV_PATH"
+    fi
+}
+
+function python_install() {
+    python_base
+    log "Installing python dependencies [$@]"
+    for requirement in "$@"; do
+        $ENV_PATH/bin/pip install -q $requirement
+    done
+}
+
+function ruby_install() {
     mkdir -p "$GEM_HOME"
-    if [ -r "$RUBY_PACKAGES" ]; then
-        log "Bootstraping ruby dependencies"
-        while read rubygem; do
-            gem install $rubygem --quiet
-        done < "$RUBY_PACKAGES"
-    fi
-fi
-if [ -r "$NODE_PACKAGES" ]; then
+    log "Bootstraping ruby dependencies"
+    for rubygem in "$@"; do
+        gem install $rubygem --quiet
+    done
+}
+
+function node_install() {
     if [ ! -x $ENV_PATH/bin/node ]; then
+        python_base
         log "Bootstrapping node"
         $ENV_PATH/bin/pip install -q nodeenv
         $ENV_PATH/bin/nodeenv -p
     fi
     source $ENV_PATH/bin/activate
-    if [ $(ls -1 "$NODE_BASE/node_modules" | wc -l) -le 1 ]; then
-        log "Bootstrapping node dependencies"
-        while read nodepkg; do
-            npm install ${nodepkg}@latest --loglevel silent --prefix $NODE_BASE
-        done < $NODE_PACKAGES
+    log "Bootstrapping node dependencies"
+    for nodepkg in "$@"; do
+        npm install ${nodepkg}@latest --loglevel silent --prefix $NODE_BASE
+    done
+}
+
+function install_dependencies() {
+    EXECUTABLE_NAME="$(basename $0)"
+    if [ ! -f "$ENV_PATH/$EXECUTABLE_NAME.installed" ]; then
+        DEPENDENCIES_LINE="$(grep "^${EXECUTABLE_NAME}:" "$DEPENDENCIES_FILE" | head -n 1)"
+        FRAMEWORK="$(echo "$DEPENDENCIES_LINE" | cut -d ' ' -f 2)"
+        FRAMEWORK_DEPENDENCIES="$(echo "$DEPENDENCIES_LINE" | cut -d ' ' -f 3-)"
+        eval "${FRAMEWORK}_install $FRAMEWORK_DEPENDENCIES"
+        touch "$ENV_PATH/$EXECUTABLE_NAME.installed"
     fi
-fi
+}
+
+install_dependencies
