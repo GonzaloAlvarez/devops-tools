e4f537e3ceaac725ea920a59837ed923c291f31e
Gonzalo Alvarez <gonzaloab@gmail.com>
2017-04-14 09:15:24 -0700

    Added more details to the status system

--- mailstatus.py
+++ mailstatus.py
@@ -3,6 +3,9 @@ import subprocess
 from urllib2 import urlopen
 from json import load
 import subprocess, os
+import requests
+from lib.conf import Configuration
+import datetime
 
 if __name__ =='__main__':
     self_path = os.path.dirname(os.path.realpath(__file__))
@@ -10,6 +13,7 @@ if __name__ =='__main__':
     env = jinja2.Environment(loader=loader)
     template = env.get_template('email-template.html')
     commands=[]
+    timenow = datetime.datetime.now().strftime("%c")
     my_env = os.environ.copy()
     my_env["PATH"] = self_path + ":" + my_env["PATH"]
     with open(self_path + '/status/commands') as f:
@@ -20,5 +24,13 @@ if __name__ =='__main__':
             commands.append({'line':'# ' + line,'output':command_output})
     host={'ip': load(urlopen('https://api.ipify.org/?format=json'))['ip']}
     output = template.render(commands=commands, host=host)
+    config = Configuration(__file__, 'config.yaml')
+    request_url = 'https://api.mailgun.net/v2/{0}/messages'.format(config.mailgun_domain)
+    request = requests.post(request_url, auth=('api', config.mailgun_apikey), data={
+        'from': 'Server Status <' + config.email_login + '>',
+        'to': config.email_recipient,
+        'subject': '[' + host['ip'] + '] Status on ' + timenow,
+        'html': output
+        })
     with open('/tmp/my_new_html_file.html', 'w') as f:
          f.write(output)
