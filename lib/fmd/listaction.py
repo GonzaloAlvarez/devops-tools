09d55c6dc8b7269c1dfd56fb8c13ba4f41ee9cc1
Gonzalo Alvarez <gonzaloab@gmail.com>
2017-09-19 11:39:18 -0700

    Added total size of files at the end of list for fcellar in verbose mode

--- lib/fmd/listaction.py
+++ lib/fmd/listaction.py
@@ -2,13 +2,23 @@ from lib.fmd.workflow import FileManagementWorkflow
 from lib.fmd.decorators import ListStage
 
 class ListAction(object):
+    @staticmethod
+    def sizeof_fmt(num, suffix='B'):
+        for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+            if abs(num) < 1024.0:
+                return "%3.1f%s%s" % (num, unit, suffix)
+            num /= 1024.0
+        return "%.1f%s%s" % (num, 'Yi', suffix)
+
     def execute(self, context):
         fadm = FileManagementWorkflow()
         data = fadm.execute(context, ListStage)
+        totalsize = 0
         if 'records' in data:
             for entry in data['records']:
                 original_filename = entry['filename_history'][0]['basename']
                 base_mime = entry['mime'].split('/')[0]
+                totalsize += int(entry['size'])
                 if context.verbose > 0:
                     rek_labels = ''
                     if 'rekognition' in entry:
@@ -16,6 +26,8 @@ class ListAction(object):
                     context.log.status('%s %s %s %s' % (entry['fid'], original_filename, base_mime, rek_labels))
                 else:
                     context.log.status('%s' % entry['fid'])
+            if context.verbose > 0:
+                context.log.status('Total size of files: %s ' % ListAction.sizeof_fmt(totalsize))
         else:
             context.log.status('Cellar does not contain any records matching your filter or it is empty')
 
