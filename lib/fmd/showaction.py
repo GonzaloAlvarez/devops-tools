0ec9afad6a7e267e07f0c3659fd1108f157257b3
Gonzalo Alvarez <gonzaloab@gmail.com>
2017-09-21 11:16:13 -0700

    FCellar added a bunch of checks for empty files and no fid to show. Added python-dev as python installed package

--- lib/fmd/showaction.py
+++ lib/fmd/showaction.py
@@ -6,5 +6,7 @@ class ShowAction(object):
     def execute(self, context):
         fadm = FileManagementWorkflow()
         output = fadm.execute(context, ShowStage)
-        context.log.status(json.dumps(output['record'], sort_keys=True, indent=4))
-
+        if 'record'in output:
+            context.log.status(json.dumps(output['record'], sort_keys=True, indent=4))
+        else:
+            context.log.status('We have found no file in the database with id [%s]' % context.fid)
--- lib/fmdplugins/size.py
+++ lib/fmdplugins/size.py
@@ -1,6 +1,12 @@
 import os
 from lib.fmd.decorators import Action,AddStage
+from lib.exceptions.workflow import EntryException, Severity
 
 @Action(AddStage.DATAGATHERING)
 def size(context, output):
-    return os.path.getsize(context.filename)
+    filesize = os.path.getsize(context.filename)
+    if filesize != None and int(filesize) > 0:
+        return filesize
+    exception = EntryException('Empty file so not uploading')
+    exception.severity = Severity.LOW
+    raise exception
